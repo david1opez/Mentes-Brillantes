5fd2ace5a13699bf33575118cd2f49a6beccfaff
David Lopez <72664248+david1opez@users.noreply.github.com>
2021-05-09 00:09:48 -0500

    🔒 SECURITY ADDED : Helmet and Express session dependencies added

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const session = require('express-session');
+
 const firebase = require("firebase/app");
 const Cryptr = require('cryptr');
 
@@ -81,11 +84,24 @@ app.use(express.json())
 app.use(express.static("dist"));
 app.use("/", router);
 
+//SECURITY
+app.use(helmet());
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: "s3Cur3",
+  name: "sessionId",
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+}));
+
 module.exports.handler = serverless(app);
 
 //REGISTER ACTIONS
 router.post('/321fc4EE-ec01-4562-8f39-93106fAef42e', (req, res) => {
     var data = req.body;
+
+    console.log(data)
 
     var action = data.action;
     var username = data.username;
@@ -93,6 +109,7 @@ router.post('/321fc4EE-ec01-4562-8f39-93106fAef42e', (req, res) => {
     var uid = data.uid;
     var grade = data.grade;
     var department = data.department;
+    var code = data.code;
 
     //IF THE USER IS NOT FROM SCHOOL
     if(action == "FOREIGN") {
@@ -188,7 +205,61 @@ router.post('/321fc4EE-ec01-4562-8f39-93106fAef42e', (req, res) => {
         )
     }
 
-    /*TODO: FIX THE PARTICIPANT REGISTER*/
+    //IF THE USER IS A PARTICIPANT FROM THE CONTEST
+    if(action == "PARTICIPANT") {
+        //CHECK IF CODE MATCHES WITH A TEAM
+        var team = null;
+
+        for(var i = 0; i < CODES.length; i++) {
+          if(code == CODES[i]){team = TEAM_NAMES[i]}
+        }
+
+        if(team != null) {
+          db.ref(`${USERS}/` + PARTICIPANTS).child(uid).set(
+            {
+              nombre: username,
+              correo: email,
+              votosRestantes: cryptr.encrypt(3),
+              votos: [
+                  {
+                    empresa: "",
+                    calificacion: {
+                        logo: cryptr.encrypt(0),
+                        eslogan: cryptr.encrypt(0),
+                        video: cryptr.encrypt(0),
+                        concepto: cryptr.encrypt(0)
+                    }
+                  },
+                  {
+                    empresa: "",
+                    calificacion: {
+                        logo: cryptr.encrypt(0),
+                        eslogan: cryptr.encrypt(0),
+                        video: cryptr.encrypt(0),
+                        concepto: cryptr.encrypt(0)
+                    }
+                  },
+                  {
+                    empresa: "",
+                    calificacion: {
+                        logo: cryptr.encrypt(0),
+                        eslogan: cryptr.encrypt(0),
+                        video: 0,
+                        concepto: cryptr.encrypt(0)
+                    }
+                  }
+                ],
+              equipo: team
+            }
+          )
+          res.json({status: "success"})
+        }
+        else {
+          res.json({status: "error"})
+        }
+        //UPLOAD USERS DATA TO THE FOREIGNS DATABASE
+        //THIS DOES THE SAME THING AS ABOVE, BUT DOES NOT INCLUDE THE SECTION AND
+    }
 });
 
 //READ USER TYPE (Participant OR Viewer)
@@ -246,24 +317,3 @@ router.post('/45b9Rf6a-c83f-44f9-a71e-c8b622Xw4d8e', (req, res) => {
   }
   catch{}
 });
-
-//TODO: MAKE THIS WORK
-router.post('/dc5FF86b9-50aa-4a30-b839-1510665d3fa', (req, res) => {
-  let logos = [];
-  let names = [];
-
-      db.ref(`${TEAMS}/` + TEAM_NAMES[0]).get().then((snapshot) => {
-        if (snapshot.exists()) {
-    
-          logos.push(snapshot.val().Logo);
-          names.push(snapshot.val().Name);
-    
-        } else {
-          console.log("No data available");
-        }
-      })
-
-      console.log(logos)
-
-  res.json({logos: logos, names: names})
-})
